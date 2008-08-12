60af829209d1dd627d19f394f26559e7aab8f89f
aleaxit <aleaxit@e104b5fd-1f54-0410-bdd9-7d987ee319b5>
2008-08-12 02:35:47 +0000

    GET in place (save for error handling!!!), GET/POST/PUT still to do

--- main.py
+++ main.py
@@ -1,50 +1,34 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import wsgiref.handlers
 from google.appengine.ext import webapp
 import models
+import jsonutil
 import restutil
 
 
-class RestHandler(webapp.RequestHandler):
+# TODO: post, put, delete, queries, and MUCH better error-handling!-)
 
-  def serve(self, stuff):
-    self.response.content_type = 'text/plain'
-    self.response.out.write(str(stuff))
+class CrudRestHandler(webapp.RequestHandler):
 
+  def _serve(self, data):
+    return jsonutil.send_json(self.response, data)
 
   def get(self):
-    path = self.request.path.strip('/')
-    if not path:
-      self.serve(restutil.allModelClassNames())
-    else:
-      cls = restutil.modelClassFromName(path)
-      if cls is None:
-        self.serve('No class named %r' % path)
-      else:
-        objs = cls.get_all()
-        n = len(objs)
-        self.serve('%s object%s of class %r' % (n, 's'[n==1:]))
+    classname, strid = jsonutil.path_to_classname_and_id(self.request.path))
+    if not classname:
+      return self._serve(restutil.allModelClassNames())
+    cls = restutil.modelClassFromName(classname)
+    if not strid:
+      return self._serve([jsonutil.id_of(x) for x in cls.all()])
+    numid = int(strid)
+    instance = cls.get_by_id(numid)
+    json_dict = jsonutil.make_json_dict(cls, instance)
+    return self._serve(json_dict)
+
 
 def main():
-  application = webapp.WSGIApplication([('/', RestHandler)],
+  application = webapp.WSGIApplication([('/', CrudRestHandler)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
-
 
 if __name__ == '__main__':
   main()
