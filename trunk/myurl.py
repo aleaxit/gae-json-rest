0dbdd44882f43f69c0008e1ba5c31710efc14ec0
atharh <atharh@e104b5fd-1f54-0410-bdd9-7d987ee319b5>
2008-08-16 21:00:03 +0000

    throwing away old code and trying out a mix-in approach

--- trunk/myurl.py
+++ trunk/myurl.py
@@ -1,25 +1,21 @@
 import django.conf.urls.defaults as d
 import pprint as pp
 import logging
+import re
 
-def index(*args, **kwargs):
-  print 'Inside index'
-
-def hello(*args, **kwargs):
-  pp.pprint(*args)
-  pp.pprint(kwargs)
-
-urlpatterns = d.patterns('',
-    (r'^$', index),
-    (r'^hello/(\d+)$', hello),
-    )
-
-def resolve(path):
-  for pattern in urlpatterns:
-    m = pattern.resolve(path)
-    if m: return m
-  return ('', (), {})
-
-if __name__ == '__main__':
-  resolve('hello/123')
-  resolve('')
+class URLHandlerMixin(object):
+  """ This class is designed to be mixed in with a WSGI app request handler.
+      
+      register_patterns should be called with the appropriate regexes that 
+      need to handled by the particular handler this class is mixed-in with.
+  """
+  
+  def register_patterns(*args):
+    self.patterns = d.patterns(*args)
+  
+  def resolve(self, re, path):
+    for pattern in patterns:
+      m = pattern.resolve(path)
+      if m:
+        return m
+      return None
