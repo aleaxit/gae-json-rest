58dc8515d1f5c80ba056000a30dc0be56619f073
aleaxit <aleaxit@e104b5fd-1f54-0410-bdd9-7d987ee319b5>
2008-08-17 22:21:40 +0000

    Made test quiet and self-checking.

--- testutil.py
+++ testutil.py
@@ -1,56 +1,84 @@
 # common needs of JSON-REST-based client-side Python tests
 # (intended to be run while gae-json-rest is being served at localhost:8080)!
 import httplib
+import optparse
+import socket
+import sys
+
 import simplejson
 
-HOST = 'localhost'
-PORT = 8080
+DEFAULT_HOST = 'localhost'
+DEFAULT_PORT = 8080
 
 
 def body(**k):
-    return simplejson.dumps(k)
+  return simplejson.dumps(k)
 
 
 def getAny(conn, classname):
-    """ Returns the ID of any one existing entity of the model, or None
-    """
-    data = silent_request(conn, 'GET', '/%s/' % classname)
-    if data: return data[0]['id']
-    else: return None
+  """ Returns the ID of any one existing entity of the model, or None
+  """
+  data = silent_request(conn, 'GET', '/%s/' % classname)
+  if data: return data[0]['id']
+  else: return None
 
 
 def silent_request(conn, verb, path, body=None):
-    """ Returns the JSON-deserialized of the response body, or None
-    """
-    if body is None: conn.request(verb, path)
-    else: conn.request(verb, path, body)
-    rl = conn.getresponse()
-    if rl.status//100 != 2: return None
-    return simplejson.loads(rl.read())
+  """ Makes an HTTP request, always silently.
+
+      Returns the JSON-deserialized of the response body, or None.
+  """
+  return request_and_show(conn, verb, path, False, body)
 
 
-def request_and_show(conn, verb, path, body=None):
-    """ Makes an HTTP request, prints data about the interaction.
-    """
+def request_and_show(conn, verb, path, verbose, body=None):
+  """ Makes an HTTP request, optionally prints data about the interaction.
+
+      Returns the JSON-deserialized of the response body, or None.
+  """
+  try:
     if body is None: conn.request(verb, path)
     else: conn.request(verb, path, body)
-    rl = conn.getresponse()
+  except socket.error, e:
+    print 'Cannot request %r %r: %s' % (verb, path, e)
+    sys.exit(1)
+  rl = conn.getresponse()
+  if verbose:
     print '%s %s gave: %s %r' % (verb, path, rl.status, rl.reason)
-    if rl.status//100 == 2:
-        print 'HEADERS:'
-        for h, v in rl.getheaders(): print ' ', h, v
-        print 'CONTENTS:'
-        body = rl.read()
-        for line in body.splitlines():
-            print ' ', line
-        print
-        return simplejson.loads(body)
-    else:
-        return None
+  if rl.status//100 == 2:
+    if verbose:
+      print 'HEADERS:'
+      for h, v in rl.getheaders(): print ' ', h, v
+      print 'CONTENTS:'
+    body = rl.read()
+    if verbose:
+      for line in body.splitlines():
+        print ' ', line
+      print
+    return simplejson.loads(body)
+  else:
+    return None
 
 
-def main(f, host=HOST, port=PORT):
-    conn = httplib.HTTPConnection(host, port, strict=True)
-    f(conn)
-    print 'All done!'
+def main(f):
+  # get command-line options
+  parser = optparse.OptionParser()
+  parser.add_option("-v", "--verbose",
+                    action="store_true", dest="verbose", default=False,
+                    help="print detailed info to stdout")
+  parser.add_option("-s", "--host", dest="host", default=DEFAULT_HOST,
+                    help="what host the server is running on")
+  parser.add_option("-p", "--port", dest="port", default=DEFAULT_PORT,
+                    type="int", help="what port the server is running on")
+  options, args = parser.parse_args()
+  if args:
+    print 'Unknown arguments:', args
+    sys.exit(1)
+  try:
+    conn = httplib.HTTPConnection(options.host, options.port, strict=True)
+  except socket.error, e:
+    print "Cannot connect: %s"
+    sys.exit(1)
+  f(conn, options.verbose)
+  print 'All done OK!'
 
