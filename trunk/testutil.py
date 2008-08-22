fe412ed8c6d9b0502926956dd3b5b730e29f3fbe
atharh <atharh@e104b5fd-1f54-0410-bdd9-7d987ee319b5>
2008-08-22 21:54:04 +0000

    A half-hearted attempt at refactoring testutil into a class. Thou shalt not make fun.

--- trunk/testutil.py
+++ trunk/testutil.py
@@ -23,62 +23,65 @@ def getAny(conn, classname):
   else: return None
 
 
-def silent_request(conn, verb, path, body=None):
-  """ Makes an HTTP request, always silently.
+class Tester(object):
+  def __init__(self, f):
+    self.f = f
 
-      Returns the JSON-deserialized of the response body, or None.
-  """
-  return request_and_show(conn, verb, path, False, body)
+  def silent_request(self, verb, path, body=None):
+    """ Makes an HTTP request, always silently.
 
-
-def request_and_show(conn, verb, path, verbose, body=None):
-  """ Makes an HTTP request, optionally prints data about the interaction.
-
-      Returns the JSON-deserialized of the response body, or None.
-  """
-  try:
-    if body is None: conn.request(verb, path)
-    else: conn.request(verb, path, body)
-  except socket.error, e:
-    print 'Cannot request %r %r: %s' % (verb, path, e)
-    sys.exit(1)
-  rl = conn.getresponse()
-  if verbose or rl.status//100 != 2:
-    print '%s %s gave: %s %r' % (verb, path, rl.status, rl.reason)
-  if rl.status//100 == 2:
-    if verbose:
-      print 'HEADERS:'
-      for h, v in rl.getheaders(): print ' ', h, v
-      print 'CONTENTS:'
-    body = rl.read()
-    if verbose:
-      for line in body.splitlines():
-        print ' ', line
-      print
-    return simplejson.loads(body)
-  else:
-    return None
+        Returns the JSON-deserialized of the response body, or None.
+    """
+    return self.request_and_show(verb, path, False, body)
 
 
-def main(f):
-  # get command-line options
-  parser = optparse.OptionParser()
-  parser.add_option("-v", "--verbose",
-                    action="store_true", dest="verbose", default=False,
-                    help="print detailed info to stdout")
-  parser.add_option("-s", "--host", dest="host", default=DEFAULT_HOST,
-                    help="what host the server is running on")
-  parser.add_option("-p", "--port", dest="port", default=DEFAULT_PORT,
-                    type="int", help="what port the server is running on")
-  options, args = parser.parse_args()
-  if args:
-    print 'Unknown arguments:', args
-    sys.exit(1)
-  try:
-    conn = httplib.HTTPConnection(options.host, options.port, strict=True)
-  except socket.error, e:
-    print "Cannot connect: %s"
-    sys.exit(1)
-  f(conn, options.verbose)
-  print 'All done OK!'
+  def request_and_show(self, verb, path, verbose, body=None):
+    """ Makes an HTTP request, optionally prints data about the interaction.
+
+        Returns the JSON-deserialized of the response body, or None.
+    """
+    try:
+      if body is None: self.conn.request(verb, path)
+      else: self.conn.request(verb, path, body)
+    except socket.error, e:
+      print 'Cannot request %r %r: %s' % (verb, path, e)
+      sys.exit(1)
+    rl = self.conn.getresponse()
+    if verbose or rl.status//100 != 2:
+      print '%s %s gave: %s %r' % (verb, path, rl.status, rl.reason)
+    if rl.status//100 == 2:
+      if verbose:
+        print 'HEADERS:'
+        for h, v in rl.getheaders(): print ' ', h, v
+        print 'CONTENTS:'
+      body = rl.read()
+      if verbose:
+        for line in body.splitlines():
+          print ' ', line
+        print
+      return simplejson.loads(body)
+    else:
+      return None
+
+  def execute(self):
+    # get command-line options
+    parser = optparse.OptionParser()
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      help="print detailed info to stdout")
+    parser.add_option("-s", "--host", dest="host", default=DEFAULT_HOST,
+                      help="what host the server is running on")
+    parser.add_option("-p", "--port", dest="port", default=DEFAULT_PORT,
+                      type="int", help="what port the server is running on")
+    options, args = parser.parse_args()
+    if args:
+      print 'Unknown arguments:', args
+      sys.exit(1)
+    try:
+      self.conn = httplib.HTTPConnection(options.host, options.port, strict=True)
+    except socket.error, e:
+      print "Cannot connect: %s"
+      sys.exit(1)
+    self.f(self, options.verbose)
+    print 'All done OK!'
 
